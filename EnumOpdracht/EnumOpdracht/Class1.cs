13fa2aaf7cf173515f1ec5d4769d5087e0384644
Davey <36044@ma-web.nl>
2023-05-15 12:43:39 +0200

    OpdrachtEnum1/2

---
+++ EnumOpdracht/EnumOpdracht/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOpdracht
+{
+    internal enum Color
+    {
+        Red, 
+        Green, 
+        Blue,
+        purple
+    }
+}
---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EnumOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Color myMainThemeColor = Color.Blue;
+            Console.WriteLine("Hello, World!");
+            if(myMainThemeColor == Color.Blue )
+            {
+                Console.WriteLine("dat is mijn kleur");
+            } 
+                
+            
+        }
+    }
+
+   
+}
